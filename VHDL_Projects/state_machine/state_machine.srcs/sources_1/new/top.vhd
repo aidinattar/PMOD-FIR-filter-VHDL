43a718f6300c8156a4a66c166eb60ba0980b3df0
c0pp1 <albertocoppi@outlook.it>
2021-11-23 17:58:03 +0100

    lab4

---
+++ VHDL_Projects/state_machine/state_machine.srcs/sources_1/new/top.vhd
@@ -0,0 +1,46 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 11/23/2021 05:48:07 PM
+-- Design Name: 
+-- Module Name: top - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity top is
+    Port ( clk : in STD_LOGIC;
+           rst : in STD_LOGIC;
+           ina : in STD_LOGIC;
+           outa : out STD_LOGIC);
+end top;
+
+architecture Behavioral of top is
+
+begin
+
+
+end Behavioral;
--- VHDL_Projects/synchronizer/synchronizer.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/webtalk/xsim_webtalk.tcl
+++ VHDL_Projects/synchronizer/synchronizer.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir /home/alberto/Desktop/MAPD/MAPD_Projects/VHDL_Projects/synchronizer/synchronizer.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Tue Nov 23 17:37:21 2021" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Tue Nov 23 17:52:52 2021" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.3 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2405991" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "LIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "cba5d349-02b9-4651-9f67-01adc5fca06b" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "4dd0e29b6c324ec08abe738c61bee665" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "7" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "8" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Ubuntu" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "Ubuntu 21.04" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i5-8300H CPU @ 2.30GHz" -context "user_environment"
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "2" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.01_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.05_sec" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Simulation_Memory -value "114812_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 1600610810 -regid "" -xml /home/alberto/Desktop/MAPD/MAPD_Projects/VHDL_Projects/synchronizer/synchronizer.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/webtalk/usage_statistics_ext_xsim.xml -html /home/alberto/Desktop/MAPD/MAPD_Projects/VHDL_Projects/synchronizer/synchronizer.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/webtalk/usage_statistics_ext_xsim.html -wdm /home/alberto/Desktop/MAPD/MAPD_Projects/VHDL_Projects/synchronizer/synchronizer.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_transmit -clientid 2317822974 -regid "" -xml /home/alberto/Desktop/MAPD/MAPD_Projects/VHDL_Projects/synchronizer/synchronizer.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/webtalk/usage_statistics_ext_xsim.xml -html /home/alberto/Desktop/MAPD/MAPD_Projects/VHDL_Projects/synchronizer/synchronizer.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/webtalk/usage_statistics_ext_xsim.html -wdm /home/alberto/Desktop/MAPD/MAPD_Projects/VHDL_Projects/synchronizer/synchronizer.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
