adf444c2e43c647cb957d19e0bf7ddeb90c22304
Aidin Attar <attaraidin@gmail.com>
2022-02-16 21:49:38 +0100

    back to 15 taps

--- final_project.srcs/sources_1/new/FIR.vhd
+++ final_project.srcs/sources_1/new/FIR.vhd
@@ -14,23 +14,23 @@ use ieee.numeric_std.all;
 
 entity fir_filter is
 generic(
-    d_width : integer := 24;
-    shift   : integer :=  7;
-    coeff1  : integer :=  3;
-    coeff2  : integer := 12;
-    coeff3  : integer := 30;
-    coeff4  : integer := 39;
-    coeff5  : integer := 30;
-    coeff6  : integer := 12;
-    coeff7  : integer :=  3);
-    --coeff8  : integer := 17;
-    --coeff9  : integer := 16;
-    --coeff10 : integer := 14;
-    --coeff11 : integer := 11;
-    --coeff12 : integer :=  7;
-    --coeff13 : integer :=  4;
-    --coeff14 : integer :=  2;
-    --coeff15 : integer :=  1);
+    d_width : integer;
+    shift   : integer;
+    coeff1  : integer;
+    coeff2  : integer;
+    coeff3  : integer;
+    coeff4  : integer;
+    coeff5  : integer;
+    coeff6  : integer;
+    coeff7  : integer;
+    coeff8  : integer;
+    coeff9  : integer;
+    coeff10 : integer;
+    coeff11 : integer;
+    coeff12 : integer;
+    coeff13 : integer;
+    coeff14 : integer;
+    coeff15 : integer);
 port(
     clk        : in  std_logic;                        -- system clock
     rst        : in  std_logic;                        -- reset
@@ -40,18 +40,18 @@ end fir_filter;
 
 architecture rtl of fir_filter is
 
-type t_data_pipe      is array (0 to 6) of signed(d_width-1     downto 0);
-type t_coeff          is array (0 to 6) of signed(7             downto 0);
-type t_mult           is array (0 to 6) of signed(8+d_width-1   downto 0);
+type t_data_pipe      is array (0 to 14) of signed(d_width-1   downto 0);
+type t_coeff          is array (0 to 14) of signed(7           downto 0);
+type t_mult           is array (0 to 14) of signed(8+d_width-1 downto 0);
 --type t_add_st0        is array (0 to 1) of signed(15+1        downto 0);
 
 signal r_coeff              : t_coeff := (to_signed(coeff1 , 8), to_signed(coeff2 , 8), to_signed(coeff3 , 8), to_signed(coeff4 , 8), to_signed(coeff5 , 8),
-                                          to_signed(coeff6 , 8), to_signed(coeff7 , 8));--, to_signed(coeff8 , 8), to_signed(coeff9 , 8), to_signed(coeff10, 8),
-                                          --to_signed(coeff11, 8), to_signed(coeff12, 8), to_signed(coeff13, 8), to_signed(coeff14, 8), to_signed(coeff15, 8));
+                                          to_signed(coeff6 , 8), to_signed(coeff7 , 8), to_signed(coeff8 , 8), to_signed(coeff9 , 8), to_signed(coeff10, 8),
+                                          to_signed(coeff11, 8), to_signed(coeff12, 8), to_signed(coeff13, 8), to_signed(coeff14, 8), to_signed(coeff15, 8));
 signal p_data               : t_data_pipe;
 signal r_mult               : t_mult;
 --signal r_add_st0            : t_add_st0;
-signal r_add_st1            : signed(14+d_width downto 0);
+signal r_add_st1            : signed(13+d_width downto 0);
 
 begin
 
@@ -92,10 +92,14 @@ begin
   if(rst='1') then
     r_add_st1     <= (others=>'0');
   elsif(rising_edge(clk)) then
-    r_add_st1 <= (others=>'0');
-    for k in 0 to r_mult'length-1 loop
-        r_add_st1 <= r_add_st1 + resize(r_mult(k),r_add_st1'length);
-    end loop;
+    r_add_st1 <= resize(r_mult(0),r_add_st1'length) + resize(r_mult(1),r_add_st1'length) +
+                 resize(r_mult(2),r_add_st1'length) + resize(r_mult(3),r_add_st1'length) +
+                 resize(r_mult(4),r_add_st1'length) + resize(r_mult(5),r_add_st1'length) +
+                 resize(r_mult(6),r_add_st1'length) + resize(r_mult(7),r_add_st1'length) +
+                 resize(r_mult(8),r_add_st1'length) + resize(r_mult(9),r_add_st1'length) +
+                 resize(r_mult(10),r_add_st1'length) + resize(r_mult(11),r_add_st1'length) +
+                 resize(r_mult(12),r_add_st1'length) + resize(r_mult(13),r_add_st1'length) +
+                 resize(r_mult(14),r_add_st1'length);
   end if;
 end process p_add_st1;
 
